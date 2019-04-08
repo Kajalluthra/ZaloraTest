268b8cb66052011bebd948e010980467a6a2e25a
Kajal <kajal@aleph-labs.com>
2019-04-08 20:50:14 +0800

    Initial commit

---
+++ ZaloraTest/ZaloraTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ZaloraTest
+//
+//  Created by Kajal on 8/4/19.
+//  Copyright © 2019 Kajal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
